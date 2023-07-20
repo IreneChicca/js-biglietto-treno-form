540d4fc6ce19b1951366258e292db4d0535f9fb2
IreneChicca <irene.chicca@outlook.com>
2023-07-20 15:23:45 +0200

    JS

---
+++ script.js
@@ -0,0 +1,56 @@
+/*
+
+- al click del bottone comparirà sulla pagina il prezzo da pagare 
+
+*/
+
+
+// - Attraverso un form raccolgo le informazioni dell'utente relative ai km da percorrere e la sua età anagrafica
+//- Raccolgo queste informazioni attraverso l'id dei vari input e le inserisco all'interno di variabili
+
+const userName = document.getElementById("name").value
+const userAge = parseInt(document.getElementById("age").value)
+const userKm = parseFloat(document.getElementById("km").value)
+
+const ticketPrice = userKm * 0.21
+
+console.log(ticketPrice)
+
+
+// - Imposto delle condizioni di sconto relative all'età dell'utente
+
+if (userAge < 18) {
+
+    ticketPrice *
+
+        console.log(ticketPrice)
+
+}
+
+else if (age > 65) {
+
+
+
+}
+
+
+// creo bottone collegato ad evento
+
+const sendBtn = document.getElementById("btn")
+
+
+// individuo campi da valorizzare dopo il click del bottone 
+
+const bglName = document.getElementById("bgl-name")
+const bglAge = document.getElementById("bgl-age")
+const bglKm = document.getElementById("bgl-km")
+const bglPrice = document.getElementById("bgl-price")
+
+sendBtn.addEventListener('click', function () {
+
+
+
+
+})
+
+
